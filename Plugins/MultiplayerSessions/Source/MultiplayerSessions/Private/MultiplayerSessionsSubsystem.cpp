753d33f6888b8f045e27fd68db591e42e362634b
Maksymilian Kafka <m.kafka00@gmail.com>
2024-04-23 20:05:07 +0200

    MultiplayerSessionsSubsystem - added OnlineSession interface functions/callbacks/delegates setup

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
@@ -6,9 +6,54 @@
 #include "OnlineSubsystem.h"
 
 UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem()
+	: CreateSessionCompleteDelegate( FOnCreateSessionCompleteDelegate::CreateUObject( this, &ThisClass::OnCreateSessionComplete ) ),
+	  FindSessionsCompleteDelegate( FOnFindSessionsCompleteDelegate::CreateUObject( this, &ThisClass::OnFindSessionsComplete ) ),
+	  JoinSessionCompleteDelegate( FOnJoinSessionCompleteDelegate::CreateUObject( this, &ThisClass::OnJoinSessionComplete ) ),
+	  StartSessionCompleteDelegate( FOnStartSessionCompleteDelegate::CreateUObject( this, &ThisClass::OnStartSessionComplete ) ),
+	  DestroySessionCompleteDelegate( FOnStartSessionCompleteDelegate::CreateUObject( this, &ThisClass::OnDestroySessionComplete ) )
 {
-	if(const IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get())
+	if (const IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get())
 	{
 		SessionInterface = Subsystem->GetSessionInterface();
 	}
+}
+
+void UMultiplayerSessionsSubsystem::CreateSession( int32 NumPublicConnections, FString MatchType )
+{
+}
+
+void UMultiplayerSessionsSubsystem::FindSessions( int32 MaxSearchResults )
+{
+}
+
+void UMultiplayerSessionsSubsystem::JoinSession( const FOnlineSessionSearchResult& SessionSearchResult )
+{
+}
+
+void UMultiplayerSessionsSubsystem::StartSession()
+{
+}
+
+void UMultiplayerSessionsSubsystem::DestroySession()
+{
+}
+
+void UMultiplayerSessionsSubsystem::OnCreateSessionComplete( FName SessionName, bool bWasSuccessful )
+{
+}
+
+void UMultiplayerSessionsSubsystem::OnFindSessionsComplete( bool bWasSuccessful )
+{
+}
+
+void UMultiplayerSessionsSubsystem::OnJoinSessionComplete( FName SessionName, EOnJoinSessionCompleteResult::Type Result )
+{
+}
+
+void UMultiplayerSessionsSubsystem::OnStartSessionComplete( FName SessionName, bool bWasSuccessful )
+{
+}
+
+void UMultiplayerSessionsSubsystem::OnDestroySessionComplete( FName SessionName, bool bWasSuccessful )
+{
 }
--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsSubsystem.h
@@ -16,13 +16,76 @@ class MULTIPLAYERSESSIONS_API UMultiplayerSessionsSubsystem : public UGameInstan
 	GENERATED_BODY()
 
 public:
-	/** Default constructor */
+	/**
+	 * Default constructor
+	 * Binds internal callbacks to delegates
+	 */
 	UMultiplayerSessionsSubsystem();
-	
+
+
+	///
+	/// To handle session functionality. The menu class will call these.
+	///
+
+	/**
+	 * Accesses subsystem and calls its create session function
+	 * 
+	 * @param NumPublicConnections Number of players that can join the session
+	 * @param MatchType Match type that will be handled by session
+	 */
+	void CreateSession( int32 NumPublicConnections, FString MatchType );
+
+	/**
+	 * Accesses subsystem and calls its find sessions function
+	 * 
+	 * @param MaxSearchResults Search limit
+	 */
+	void FindSessions( int32 MaxSearchResults );
+
+	/**
+	 * Accesses subsystem adn calls its join session function
+	 * 
+	 * @param SessionSearchResult Found session to join
+	 */
+	void JoinSession( const FOnlineSessionSearchResult& SessionSearchResult );
+
+	/** Accesses subsystem adn calls its start function */
+	void StartSession();
+
+	/** Accesses subsystem adn calls its destroy function */
+	void DestroySession();
+
 protected:
-	
+	///
+	/// Internal callbacks for the delegates we'll add to the Online Session Interface list.
+	/// These don't need to be called outside this class.
+	///
+	void OnCreateSessionComplete( FName SessionName, bool bWasSuccessful );
+	void OnFindSessionsComplete( bool bWasSuccessful );
+	void OnJoinSessionComplete( FName SessionName, EOnJoinSessionCompleteResult::Type Result );
+	void OnStartSessionComplete( FName SessionName, bool bWasSuccessful );
+	void OnDestroySessionComplete( FName SessionName, bool bWasSuccessful );
 
 private:
 	/** Used to handle sessions */
 	IOnlineSessionPtr SessionInterface;
+
+	///
+	/// To add to Online Session Interface delegate list.
+	/// We'll bind our MultiplayerSessionSubsystem internal callbacks to these.
+	///
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+
+	///
+	/// Delegate handles for delegates above
+	///
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	FDelegateHandle FindSessionsCompleteDelegateHandle;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
 };
